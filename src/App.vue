d070816da7ce28e6dec317adb595555a4a2e078d
Mikkel Svartveit <mikkel@cloudinsurance.io>
2021-06-15 15:36:14 +0200

    created todo app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,140 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Vue-Do</h1>
+
+    <div id="content">
+      <div id="add-todo">
+        <h2>Add Todo</h2>
+        <div>
+          <input v-on:keyup.enter="addTodo" type="text" v-model="newTodoText" />
+          <button v-on:click="addTodo">Add</button>
+        </div>
+      </div>
+
+      <div class="todos">
+        <h2>Your Todos</h2>
+
+        <div
+          v-bind:class="{ todoItem: true, checked: todo.finished }"
+          v-for="todo in todos"
+          v-bind:key="todo.id"
+        >
+          <div>
+            <input type="checkbox" v-model="todo.finished" />
+            <span>{{ todo.text }}</span>
+          </div>
+
+          <button @click="deleteTodo(todo.id)">X</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Todos from './components/Todos.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Todos,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  newTodoText = '';
+
+  todos: { id: number; text: string; finished: boolean }[] = [
+    { id: 1, text: 'First item', finished: false },
+    { id: 2, text: 'Second item', finished: true },
+  ];
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  addTodo(): void {
+    this.todos.push({ id: this.todos.length + 1, text: this.newTodoText, finished: false });
+    this.newTodoText = '';
+  }
+}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #333;
   margin-top: 60px;
+  font-size: 18px;
+  line-height: 1.6;
+}
+
+h1 {
+  text-align: center;
+  font-size: 54px;
+  font-weight: 300;
+}
+
+h2 {
+  font-weight: 400;
+  margin-top: 60px;
+}
+
+#content {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+#add-todo div {
+  display: flex;
+  justify-content: stretch;
+}
+
+#add-todo div input {
+  flex-grow: 1;
+}
+
+input {
+  font-size: inherit;
+  font-family: inherit;
+  padding: 8px;
+  border: 1px solid #888;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+button {
+  font-size: inherit;
+  font-family: inherit;
+  background-color: transparent;
+  border: 1px solid #888;
+  border-radius: 5px;
+  padding: 8px;
+}
+
+.todoItem {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #bbb;
+  padding: 10px 0;
+}
+
+.todoItem input {
+  margin: 0px 10px;
+}
+
+.todoItem button {
+  background-color: transparent;
+  border: none;
+  color: #888;
+  padding: 0 10px;
+}
+
+.checked span {
+  text-decoration: line-through;
+  color: #aaa;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="todos"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Todos extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
